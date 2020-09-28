13621b837678f2e71f8320cd984e383d45b2b2ea
Bruno Kriguer Brunetti Peres <56029691+AmazingKrigor@users.noreply.github.com>
2020-09-28 12:40:17 -0300

    Add files via upload

---
+++ Atividades_Bruno_Kriguer/Ativ5/Ativ5_Operacoes.js
@@ -0,0 +1,19 @@
+    var n1, n2, soma;
+    
+    n1 = parseInt(prompt("Digite um numero n1"));
+    n2 = parseInt(prompt("Digite um numero n2"));
+
+    resultado = n1+n2;    
+    alert("A soma deles é " + resultado);
+    
+    resultado = n1-n2;
+    alert("A subtração deles é " + resultado);
+    
+    resultado = n1*n2;
+    alert("O produto deles é " + resultado);
+    
+    resultado = n1/n2;
+    alert("A divisao do primeiro pelo segundo é " + resultado);
+    
+    resultado = n1%n2;
+    alert("O resto do primeiro pelo segundo é " + resultado);
