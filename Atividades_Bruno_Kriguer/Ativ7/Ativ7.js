37976c89b06aa88c6f66696dbb9158d7297f1146
Bruno Kriguer Brunetti Peres <56029691+AmazingKrigor@users.noreply.github.com>
2020-10-19 08:14:00 -0800

    Add files via upload

---
+++ Atividades_Bruno_Kriguer/Ativ7/Ativ7.js
@@ -0,0 +1,19 @@
+var n1,n2,n3;
+
+n1 = parseInt(prompt("Digite o n1"));
+n2 = parseInt(prompt("Digite o n2"));
+n3 = parseInt(prompt("Digite o n3"));
+
+
+alert("O maior numero eh o " + Math.max(n1,n2,n3));
+
+
+
+if(n1 == n2){
+alert(n1);}
+
+else if(n1 == n3){
+alert(n1);}
+
+else if(n2 == n3){
+alert(n2);}
