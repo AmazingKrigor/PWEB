4d673b28e0fb949b9dd00294b79e42fe3d6d4b96
Bruno Kriguer Brunetti Peres <56029691+AmazingKrigor@users.noreply.github.com>
2020-10-19 07:33:47 -0800

    Add files via upload

---
+++ Atividades_Bruno_Kriguer/Ativ8/Ativ8.js
@@ -0,0 +1,25 @@
+    var aluno1 = {
+        
+        nome: 'Pedro',
+        idade: 25,
+        ra: 123456
+    };
+    
+    alert(aluno1.nome + "\n" + aluno1.ra + "\n" + aluno1.idade);
+
+    var aluno2 = new Object();
+    aluno2.nome = "Maria";
+    aluno2.idade = '22';
+    aluno2.ra = 123457;
+    
+    
+    alert(aluno2.nome + "\n" + aluno2.ra + "\n" + aluno2.idade);
+
+
+    var aluno3 = {};
+    aluno3.nome = "Caio";
+    aluno3.idade = '18';
+    aluno3.ra = 123458;
+    
+    
+    alert(aluno3.nome + "\n" + aluno3.ra + "\n" + aluno3.idade);
